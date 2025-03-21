b4790e47d18b0be537d8f0a1cef24ac27b64f944
Tushar-Manyal <manyaltushar@gmail.com>
2025-03-21 23:58:20 +0530

    Updated website with new changes

--- src/api/GetApiData.jsx
+++ src/api/GetApiData.jsx
@@ -1,7 +1,7 @@
 
 export const getMoviesData = async() => {
     try{
-       const response=await fetch(`http://www.omdbapi.com/?i=tt3896198&apikey=${import.meta.env.VITE_APIKEY}&s=titanic&page=1`);
+       const response=await fetch(`https://www.omdbapi.com/?i=tt3896198&apikey=${import.meta.env.VITE_APIKEY}&s=titanic&page=1`);
        const data=response.json();
        return data;
     }catch(error){
--- src/api/GetMoviesDetails.jsx
+++ src/api/GetMoviesDetails.jsx
@@ -3,7 +3,7 @@ export const getMoviesDetails = async({params}) => {
     console.log(params);
     const id=params.moviesID
     try{
-       const response=await fetch(`http://www.omdbapi.com/?i=${id}&apikey=${import.meta.env.VITE_APIKEY}`);
+       const response=await fetch(`https://www.omdbapi.com/?i=${id}&apikey=${import.meta.env.VITE_APIKEY}`);
        const data=response.json();
        return data;
     }catch(error){
